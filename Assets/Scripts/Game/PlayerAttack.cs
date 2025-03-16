5b051aba03b0fb8a29bce7e83e2fc0938eab0466
RCodeSoligorsk <robosolig@gmail.com>
2025-03-16 15:47:05 +0300

    Added target marker

--- Assets/Scripts/Game/PlayerAttack.cs
+++ Assets/Scripts/Game/PlayerAttack.cs
@@ -16,6 +16,8 @@ public class PlayerAttack : MonoBehaviour
 
     private bool _isAiming;
 
+    private TargetMark _target;
+
     private void OnEnable()
     {
         _playerControls.Enable();
@@ -54,6 +56,11 @@ public class PlayerAttack : MonoBehaviour
     {
         _isAiming = context.ReadValueAsButton();
         _trailTransform.gameObject.SetActive(_isAiming);
+
+        if (_target != null && _isAiming == false)
+        {
+            _target.GetComponent<MeshRenderer>().enabled = false;
+        }
     }
 
     private void OnShoot(InputAction.CallbackContext context)
@@ -75,16 +82,26 @@ public class PlayerAttack : MonoBehaviour
                 _trailTransform.forward = direction;
             }
 
-            RaycastHit[] raycastHits = Physics.RaycastAll(_trailTransform.position + _trailTransform.forward, _trailTransform.forward, 8.0f);
             Debug.DrawRay(_trailTransform.position + _trailTransform.forward, _trailTransform.forward * 8, Color.red);
 
-            foreach (RaycastHit hit in raycastHits)
+            if (Physics.Raycast(_trailTransform.position, _trailTransform.forward, out RaycastHit hit, 8.0f))
             {
                 if (hit.collider.TryGetComponent(out TargetMark targetMark))
                 {
-                    targetMark.IsTarget = true;
-                    Debug.Log("SHOWED");
+                    if (_target != targetMark && _target != null)
+                        _target.GetComponent<MeshRenderer>().enabled = false;
+
+                    _target = targetMark;
+                    _target.GetComponent<MeshRenderer>().enabled = true;
                 }
+                else if (_target != null)
+                {
+                    _target.GetComponent<MeshRenderer>().enabled = false;
+                }
+            }
+            else if (_target != null)
+            {
+                _target.GetComponent<MeshRenderer>().enabled = false;
             }
         }
     }
--- Assets/Scripts/Game/TargetMark.cs
+++ Assets/Scripts/Game/TargetMark.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class TargetMark : MonoBehaviour
 {
-    public bool IsTarget { get; set; } = false;
-
-    private MeshRenderer _meshRenderer;
-
-    private void Start()
-    {
-        _meshRenderer = GetComponent<MeshRenderer>();
-    }
-
-    private void Update()
-    {
-        _meshRenderer.enabled = false;
-    }
-
-    private void LateUpdate()
-    {
-        _meshRenderer.enabled = IsTarget;
-        IsTarget = false;
-    }
+    
 }
